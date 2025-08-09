366d9878569865b3a73bef8c2ed26ed1df80e923
Aswin Haridas <163425830+aswin-haridas@users.noreply.github.com>
2025-08-09 22:26:07 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://medusa-ewmh.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default api;
