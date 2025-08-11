8e3b7f84c9deb6273bf4bcf8009be1185b33b73e
Aswin Haridas <163425830+aswin-haridas@users.noreply.github.com>
2025-08-11 19:39:35 +0000

    olllama

--- src/Void.jsx
+++ src/Void.jsx
@@ -1,12 +1,6 @@
 const Void = () => {
   return (
-    <a
-      href={"https://void-beryl-theta.vercel.app/"}
-      target={"_blank"}
-      className={"void"}
-    >
-      Void
-    </a>
+    
   );
 };
 
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://medusa-ewmh.onrender.com',
+  baseURL: 'https://lightning.ai/aswinharidas0/playground/studios/litdata/web-ui?port%3D8000',
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 
 export default api;
