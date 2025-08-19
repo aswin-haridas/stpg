9ee3f996e7595e70201d751155663a3f4bb980e2
Aswin Haridas <163425830+aswin-haridas@users.noreply.github.com>
2025-08-19 10:59:43 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lightning.ai/aswinharidas0/playground/studios/litdata/web-ui?port%3D8000',
+  baseURL: 'https://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
+
